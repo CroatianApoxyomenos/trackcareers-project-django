c2550af345ded39147f979ed52b611764e210487
CroatianApoxyomenos <george.daniel18@outlook.com>
2023-03-02 18:03:38 +0200

    Django template language practice

--- app/views.py
+++ app/views.py
@@ -1,34 +1,40 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseNotFound
 from django.urls import reverse
 
 
 job_title = [
     "First job",
-    "Second job"
+    "Second job",
+    "Third job",
 ]
 
 job_description = [
     "First job description",
-    "Second job description"
+    "Second job description",
+    "Third job description"
 ]
 
 # Create your views here.
+class TempClass:
+    x = 5
+
+def hello(request):
+    list = ["alpha", "beta"]
+    is_authenticated = False
+    context = {"name": "George", "first_list": list, "temp_object": TempClass(), "age": 25, "is_authenticated": is_authenticated}
+    return render(request, 'app/hello.html', context)
+
 
 def job_list(request):
-    list_of_jobs = "<ul>"
-    for job in job_title:
-        job_id = job_title.index(job)+1
-        detail_url = reverse('jobs_detail', args=(job_id,))
-        list_of_jobs += f"<li>{job} <a href={detail_url}>here</a></li>"
-    list_of_jobs += "</ul>"
-    return HttpResponse(list_of_jobs)
+    context = {"job_list": job_title}
+    return render(request, 'app/index.html', context)
 
 def job_detail(request, id):
     try:
         if id == 0:
             return redirect(reverse('jobs_home'))
-        return_html = f"<h1>{job_title[id-1]}</h1> <h3>{job_description[id-1]}</h3>"
-        return HttpResponse(return_html)
+        context = {"job_title": job_title[id], "job_description": job_description[id]}
+        return render(request, 'app/job_detail.html', context)
     except:
         return HttpResponseNotFound("Not found")
