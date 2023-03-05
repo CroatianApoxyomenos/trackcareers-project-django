dabae2814477906a5bb996e38ab41b7fb214ed68
CroatianApoxyomenos <george.daniel18@outlook.com>
2023-03-05 18:08:57 +0200

    Registered the models with Django Admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from app.models import JobPost
+
+
 # Register your models here.
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('title', 'salary', 'date')
+    list_filter = ('date', 'salary', 'expiry')
+    search_fields = ('title', 'description')
+    fieldsets = (
+        ('Basic information', {
+            'fields' : ('title', 'description')
+        }),
+        ('More information', {
+            'fields': ('expiry', 'salary', 'slug')
+        })
+    )
+
+
+admin.site.register(JobPost, JobAdmin)
